1db11e3a1c7fd4477137d804f6c8dc1295866692
Ahmed Taha <ahmdtaha@cs.umd.edu>
2019-08-18 07:53:03 -0700

    update config file and provide installation guidelines

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Focus_chrome_extension
-A Chrome extension that blocks social media links during a certain schedule, e.g, Weekday 8-5
+A Chrome extension that blocks social media links during a certain schedule, e.g, Weekday 8-5. Inspired by [Focus](https://heyfocus.com/), I decided to make a free version with simpler options. The current version is not user friendly, but is straight forward for someone with basic computer skills.
+
+## How to use it
+
+* Download and extract this repos :)
+* [Install the chrome extension manually](https://www.youtube.com/watch?v=CmXJOWeecvc). Basically, (1) enable the developer mode, (2) load unpacked extension
+* Update [config.js](https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/config.js) to match your preference
+
--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Weekend","Weekday"
 var globals = [
 
-["facebook.com","Weekend","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
-["twitter.com","Weekend","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["facebook.com","Weekend","5:00AM-5:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["twitter.com","Weekend","5:00AM-5:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
 
-["facebook.com","Weekday","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
-["twitter.com","Weekday","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["facebook.com","Weekday","5:00AM-5:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["twitter.com","Weekday","5:00AM-5:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
 
 // add new row if you want add more rules
 
