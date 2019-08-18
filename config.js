6aa7009674bd50c814da5876c3f8ec51e8a42824
Ahmed Taha <ahmdtaha@cs.umd.edu>
2019-08-18 07:45:06 -0700

    update config to load html from github

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 // "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday","Weekend","Weekday"
 var globals = [
 
-["facebook.com","Weekend","5:00AM-7:00PM","http://www.cs.umd.edu/~ahmdtaha/template.html"],// add new row if you want add more rules
-["twitter.com","Weekend","5:00AM-7:00PM","http://www.cs.umd.edu/~ahmdtaha/template.html"],// add new row if you want add more rules
+["facebook.com","Weekend","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["twitter.com","Weekend","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
 
-["facebook.com","Weekday","5:00AM-7:00PM","http://www.cs.umd.edu/~ahmdtaha/template.html"],// add new row if you want add more rules
-["twitter.com","Weekday","5:00AM-7:00PM","http://www.cs.umd.edu/~ahmdtaha/template.html"],// add new row if you want add more rules
+["facebook.com","Weekday","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
+["twitter.com","Weekday","5:00AM-7:00PM","http://htmlpreview.github.io/?https://github.com/ahmdtaha/Focus_chrome_extension/blob/master/template.html"],
 
+// add new row if you want add more rules
 
 ]
